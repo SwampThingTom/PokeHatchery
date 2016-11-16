a5be3ef7373167d5654cf5f89a955fe8d431be35
Thomas Aylesworth <thomas.aylesworth@gmail.com>
2016-11-15 20:14:04 -0500

    Initial Commit

---
+++ PokeHatchery/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PokeHatchery
+//
+//  Created by Thomas Aylesworth on 11/15/16.
+//  Copyright © 2016 Thomas H Aylesworth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
